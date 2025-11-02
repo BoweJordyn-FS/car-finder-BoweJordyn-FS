084ca435d4de1aa48db3aa9dd91143d7d4e0d558
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2025-11-02 10:16:27 -0500

    Added SCSS and Data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 // Imports your SCSS stylesheet
-import './styles/index.scss';
+import "./styles/index.scss";
+
+const year = document.getElementById("year");
+const make = document.getElementById("make");
+const model = document.getElementById("model");
+
+let carData = [];
+
+// Fetch car data from the JSON file
+fetch("car-dataset.json")
+  .then((response) => response.json())
+  .then((data) => {
+    carData = data;
+    populateYearSelect();
+  })
+  .catch((error) => console.error("Error fetching car data:", error));
