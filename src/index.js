7dda6a37dfc3484f7e479621b64e7bd4a7fad012
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2025-11-02 11:24:36 -0500

    Add JavaScript
    fetched data
    User lands on page – Year dropdown is active and make and model are disabled.
    
    User selects a year – Make dropdown becomes active, filtered by that year.
    
    User selects a make – Model dropdown becomes active filtered by year and make.
    
    User selects a model – The full car details are printed to the console.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,70 @@ fetch("car-dataset.json")
   .then((response) => response.json())
   .then((data) => {
     carData = data;
-    populateYearSelect();
-  })
-  .catch((error) => console.error("Error fetching car data:", error));
+    addYears();
+  });
+
+const addYears = (data) => {
+  const years = [...new Set(data.map((car) => car.year))];
+  years.forEach((yearValue) => {
+    const option = document.createElement("option");
+    option.value = yearValue;
+    option.textContent = yearValue;
+    year.appendChild(option);
+  });
+};
+// When a year is selected, enable the make dropdown
+year.addEventListener("change", () => {
+  const selectedYear = year.value;
+  make.disabled = false;
+  make.innerHTML = '<option value="">Select Make</option>';
+
+  const makes = [
+    ...new Set(
+      carData.filter((car) => car.year == year.value).map((car) => car.make)
+    ),
+  ];
+  makes.forEach((makeValue) => {
+    const option = document.createElement("option");
+    option.value = makeValue;
+    option.textContent = makeValue;
+    make.appendChild(option);
+  });
+  model.disabled = true;
+  model.innerHTML = '<option value="">Select Model</option>';
+});
+
+//when a make is selected, enable the model dropdown
+make.addEventListener("change", () => {
+  const selectedMake = make.value;
+  const selectedYear = year.value;
+  model.disabled = false;
+  model.innerHTML = '<option value="">Select Model</option>';
+
+  const models = [
+    ...new Set(
+      carData
+        .filter((car) => car.year == selectedYear && car.make == selectedMake)
+        .map((car) => car.model)
+    ),
+  ];
+  models.forEach((modelValue) => {
+    const option = document.createElement("option");
+    option.value = modelValue;
+    option.textContent = modelValue;
+    model.appendChild(option);
+  });
+});
+model.addEventListener("change", () => {
+  const selectedModel = model.value;
+  const selectedMake = make.value;
+  const selectedYear = year.value;
+
+  const selectedCar = carData.find(
+    (car) =>
+      car.year == selectedYear &&
+      car.make == selectedMake &&
+      car.model == selectedModel
+  );
+  console.log("Selected Car:", selectedCar);
+});
